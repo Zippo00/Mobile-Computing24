23cf24d8bff8d5c011903bfa40d240710a0eb9e8
Zippo00 <mikko.lempinen@student.oulu.fi>
2024-01-25 14:13:40 +0200

    HW2 Done.

--- app/src/main/java/com/example/mobilecomputing2024/MainActivity.kt
+++ app/src/main/java/com/example/mobilecomputing2024/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.mobilecomputing2024
 
+import android.annotation.SuppressLint
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
@@ -10,10 +12,14 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -21,20 +27,30 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.mobilecomputing2024.ui.theme.MobileComputing2024Theme
 
 class MainActivity : ComponentActivity() {
@@ -43,13 +59,81 @@ class MainActivity : ComponentActivity() {
         setContent {
             MobileComputing2024Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Conversation(SampleData.conversationSample)
+                Surface {
+                    //Conversation(SampleData.conversationSample)
+                    Navigation()
                 }
             }
         }
     }
 }
+
+sealed class Screen(val route: String) {
+    object MainScreen : Screen("main_screen")
+    object ConversationScreen : Screen("conversation_screen")
+}
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.MainScreen.route) {
+        composable(route = Screen.MainScreen.route) {
+            MainScreen(navController = navController)
+        }
+        composable(
+            route = Screen.ConversationScreen.route + "?name={name}",
+            arguments = listOf(
+                navArgument("name") {
+                    type = NavType.StringType
+                    defaultValue = "Mikko"
+                    nullable = true
+                }
+            )
+        ) {entry ->
+            ConversationScreen(navController = navController,name = entry.arguments?.getString("name"))
+        }
+    }
+}
+@Composable
+fun MainScreen(navController: NavController) {
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 50.dp)
+    ) {
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                      navController.navigate(Screen.ConversationScreen.route)
+            },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Messages")
+        }
+    }
+    // Exit to home screen when pressing "Back"-button
+    val activity = LocalContext.current as ComponentActivity
+    BackHandler(onBack = {
+        activity.finish() })
+}
+@Composable
+fun ConversationScreen(navController: NavController, name: String?) {
+    Box(modifier = Modifier.fillMaxWidth(1f)){
+        Conversation(SampleData.conversationSample)
+        Button(
+            onClick = { navController.navigate(Screen.MainScreen.route) },
+            modifier = Modifier.align(Alignment.BottomEnd).padding(12.dp)
+        ) {
+            Text(text = "Menu")
+        }
+
+    }
+}
+
+
 
 data class Message(val author: String, val body: String)
 
@@ -109,27 +193,13 @@ fun Conversation(messages: List<Message>) {
 }
 
 @Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
 @Composable
 fun PreviewConversation() {
     MobileComputing2024Theme {
         Surface {
-            Conversation(SampleData.conversationSample)
+            //Conversation(SampleData.conversationSample)
+            Navigation()
         }
     }
 }
-/*
-fun MessageCardPreview() {
-    MobileComputing2024Theme {
-        Surface {
-            MessageCard(
-                msg = Message("Mikko", "just setting up my msgcard.")
-            )
-        }
-    }
-}
-*/
+
